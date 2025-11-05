ac04c6fc0c265af1f5d5d2c7056dd2d9900f3f04
svilen.stoyanov <svilen.stoyanov@brandmaker.com>
2025-11-05 11:02:38 +0200

    Added docker-compose examples

--- mj-integration-tests/src/test/java/com/svistoyanov/mj/docker/utils/ContainerConstants.java
+++ mj-integration-tests/src/test/java/com/svistoyanov/mj/docker/utils/ContainerConstants.java
@@ -3,7 +3,7 @@ package com.svistoyanov.mj.docker.utils;
 public interface ContainerConstants {
 
     interface Mysql8 {
-        String IMAGE_NAME = "docker.dev.brandmaker.com/cs/mysql";
+        String IMAGE_NAME = "docker.dev.svistoyanov.com/messen-j-mysql";
         String VERSION    = "8.0.33";
         int    DB_PORT    = 3306;
     }
